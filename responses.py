025734a9a9127eee0ffff28232cf6d2c49537195
Tasnim Tabassum <30961526+Towmony@users.noreply.github.com>
2022-03-14 22:34:01 +0100

    Function for getting responses

---
+++ responses.py
@@ -0,0 +1,13 @@
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def chatbot_response(text):
+    ints = predict_class(text, model)
+    res = getResponse(ints, intents)
+    return res
